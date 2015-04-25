ee25ef2fe27b94c33484e950a3acf6e94e7e4d71
(York)Yuefeng Huang <york.y.huang@gmail.com>
2015-04-25 22:20:09 +1000

    Course project

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# GetCleanDataPrj
+# Course Project 2
+## Tasks
+The script run_analysis.R is the R script for course project 2. It implements the following steps.
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## Implementation
+
+* The data\_dir variable has the root directory of the input dataset. One can change data_dir to suit the actual data folder.
+* It first reads in all the datasets except those in the folder "Inertial Signals" because data in this folder will be excluded from the
+final dataset anyway.
+* Function cbind() is used to bind the subject, activity and feature dataset for train and test data respectively. Then function
+rbind() is used to combined the train and test datasets.
+* Regular expression "mean[(][)]|std[(][)]" is used to extract features that contain either "mean()" or "std()".
+This regular expression can be changed to add or remove selected features.
+* Function merge() is used to join main data frame with the activity label data frame on the activity_id column.
+* Function names() is used to assign the proper names for every variables. Note that "[]" is removed from the variable names.
+* Function aggregate() is used to split the dataset by subject and activity, and then carry out mean() on every variables
+except subject, activity_id and activity.
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+
+##
+##  Read all files
+##
+data_dir <- "./getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/"
+
+# Read train data
+subject_train <- read.table(paste(data_dir, "train/subject_train.txt", sep="")
+                            , header=FALSE)
+y_train <- read.table(paste(data_dir, "train/y_train.txt", sep="")
+                      , header=FALSE)
+x_train <- read.table(paste(data_dir, "train/X_train.txt", sep="")
+                      , header=FALSE)
+# Read test data
+subject_test <- read.table(paste(data_dir, "test/subject_test.txt", sep="")
+                           , header=FALSE)
+y_test <- read.table(paste(data_dir, "test/y_test.txt", sep="")
+                     , header=FALSE)
+x_test <- read.table(paste(data_dir, "test/X_test.txt", sep="")
+                     , header=FALSE)
+
+# Read meta data
+activity_labels <- read.table(paste(data_dir, "activity_labels.txt", sep="")
+                              , header=FALSE
+                              , col.names = c("activity_id","activity")
+                              , as.is = c("activity_id","activity"))
+features <- read.table(paste(data_dir, "features.txt", sep="")
+                       , header=FALSE
+                       , col.names=c("id","name")
+                       , as.is=c("id","name"))
+
+##
+##Step 1 - Merges the training and the test sets to create one data set.
+##
+dall <- rbind(cbind(subject_train, y_train, x_train)
+          , cbind(subject_test, y_test, x_test))
+
+##
+##Step 2 - Extracts only the measurements on the mean and standard deviation 
+##         for each measurement. 
+## Only extract features which has mean() or std() in its label. This can be
+## changed to add or remove features by modifying the regular expression in
+## the grepl function.
+
+selected_cols <- grepl("mean[(][)]|std[(][)]", features$name)
+
+# The 1st and the 2nd columns are subject and activity_id
+dsel <- dall[, c(TRUE,TRUE,selected_cols)]
+
+##
+##Step 3 - Uses descriptive activity names to name the activities in the data 
+## set
+##
+dlbl <- merge(dsel, activity_labels, by.x=2, by.y=1 )
+
+##
+##Step 4 - Appropriately labels the data set with descriptive variable names. 
+##
+names(dlbl) <- c("activity_id", "subject",
+                          gsub("[(][)]", "", features[selected_cols,"name"])
+                 , "activity")
+
+##
+##Step 5 - creates a second, independent tidy data set with the average of each 
+## variable for each activity and each subject.
+##
+dmean <- with(dlbl, aggregate( 
+  dlbl[, !names(dlbl) %in% c("subject","activity","activity_id")], 
+  list(subject=subject, activity=activity), mean))
+
+
+write.table(dmean, file="tidy_data.txt", row.names=FALSE)
+
+
